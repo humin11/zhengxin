0ee1c988af5a0f8d210ec872be3baebba7cd97e1
胡敏 <humin@raysdata.com>
2017-01-06 00:25:47 +0800

    restore deploy.sh for production

---
+++ .trivis/deploy.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+ssh-keyscan -H -p 4891 122.115.52.16  >> ~/.ssh/known_hosts
+scp -i .travis/cloud.key -P 4891 -r www/* root@122.115.52.16:/usr/share/nginx/html/
+ssh -i .travis/cloud.key -p 4891 root@122.115.52.16 systemctl restart nginx
