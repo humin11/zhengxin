7206a87191a04280e8c3738cee5d435df711f190
胡敏 <humin@raysdata.com>
2017-01-06 10:41:13 +0800

    chmod 0644 for cloud.key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 ssh-keyscan -H -p 4891 122.115.52.16  >> ~/.ssh/known_hosts
+chmod 0644 .travis/cloud.key
 scp -i .travis/cloud.key -P 4891 -r www/* root@122.115.52.16:/usr/share/nginx/html/
 ssh -i .travis/cloud.key -p 4891 root@122.115.52.16 systemctl restart nginx
